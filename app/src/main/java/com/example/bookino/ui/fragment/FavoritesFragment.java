9db7961b27933605254d0d55d48db6e976da3099
Setare <setareravand@gmail.com>
2023-03-29 14:58:09 +0430

    init commit

---
+++ app/src/main/java/com/example/bookino/ui/fragment/FavoritesFragment.java
@@ -0,0 +1,30 @@
+package com.example.bookino.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.bookino.base.BaseFragment;
+import com.example.bookino.databinding.FragmentFavoritesBinding;
+
+public class FavoritesFragment extends BaseFragment {
+
+    private FragmentFavoritesBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/bookino/ui/fragment/HomeFragment.java
@@ -0,0 +1,30 @@
+package com.example.bookino.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.bookino.base.BaseFragment;
+import com.example.bookino.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends BaseFragment {
+
+    private FragmentHomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater , container , false );
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
